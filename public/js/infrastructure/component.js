3ef7cbf3eb8e4d927e31c26c6e23d7700f57d941
Nacho <nandawtek@gmail.com>
2017-04-27 00:56:43 +0200

    add jwt parser to component.js

--- public/js/infrastructure/component.js
+++ public/js/infrastructure/component.js
@@ -5,7 +5,14 @@ Class('Component', {
         this.subscribe();
     },
 
+    parseJwt: function(token, callback) {
+        var base64Url = token.split('.')[1];
+        var base64 = base64Url.replace('-', '+').replace('_', '/');
+        callback(JSON.parse(window.atob(base64)));
+    },
+
     subscribe: function() {
+        Bus.subscribe('component.jwt.token', this.parseJwt.bind(this));
         console.error(this.toString() + ' not subscribed!, implement subscribe method');
     }
 
